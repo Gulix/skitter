4808feea2b8ce8776bfae906ce0cd15f250eaefd
gulix <gulix33xp@yahoo.fr>
2015-02-17 11:51:29 +0100

    Ajout d'une nouvelle interface, IParticipant, permettant de gérer les participants de manière uniforme (tournoi Solo ou par Equipe) Ajout d'un fichier de configuration générale du Tournoi

---
+++ Skitter/Skitter.Object/ConfigurationTournoi.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Skitter.Object
+{
+    [Serializable]
+    public class ConfigurationTournoi
+    {
+        #region Variables
+        eTypeParticipantTournoi _typParticipantTournoi;
+        int? _iNbCoachesParEquipe;
+        int _iNbRondes;
+        #endregion
+
+        #region Type de participants au tournoi
+        public enum eTypeParticipantTournoi
+        {
+            Solo,
+            Equipe
+        }
+
+        public eTypeParticipantTournoi TypeParticipantTournoi
+        {
+            get { return _typParticipantTournoi; }
+            set { _typParticipantTournoi = value; }
+        }
+        #endregion
+
+        #region Divers accesseurs
+        public int? NbCoachesParEquipe
+        {
+            get { return _iNbCoachesParEquipe; }
+            set { _iNbCoachesParEquipe = value; }
+        }
+
+        public int NbRondes
+        {
+            get { return _iNbRondes; }
+            set { _iNbRondes = value; }
+        }
+        #endregion
+
+        #region Constructeur
+        public ConfigurationTournoi()
+        {
+            _typParticipantTournoi = eTypeParticipantTournoi.Equipe;
+            _iNbCoachesParEquipe = 3;
+            _iNbRondes = 5;
+        }
+        #endregion
+
+        
+    }
+}
---
+++ Skitter/Skitter.Object/Interfaces/IParticipant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Skitter.Object.Interfaces
+{
+    /// <summary>
+    /// Un IParticipant est une entité participant à un tournoi, et apparaissant dans le classement.
+    /// Globalement, il s'agit de pourvoir une classe parente pour gérer les participants à un tournoi solo ou par équipe.
+    /// </summary>
+    public interface IParticipant
+    {
+        string NomParticipant { get; set; }
+        int IdParticipant { get; }
+        List<Coach> ListeCoaches { get; }
+        string GetErreurs();
+    }
+}
